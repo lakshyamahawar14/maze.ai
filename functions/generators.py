263baf4f05d708905c3997017189efbe38ad8223
Lakshya Mahawar <83651022+lakshyamahawar14@users.noreply.github.com>
2023-02-20 13:27:35 +0530

    added solvability

--- functions/generators.py
+++ functions/generators.py
@@ -18,6 +18,19 @@ class Generators:
 					
 		return levelMatrix
 
+	def findUniquePathsHelper(self, i, j, r, c, levelMatrix):
+		if(i == r or j == c):
+			return 0
+		if(levelMatrix[i][j] != 0):
+			return 0
+		if(i == r-1 and j == c-1):
+			return 1
+		return  self.findUniquePathsHelper(i+1, j, r, c, levelMatrix) + self.findUniquePathsHelper(i, j+1, r, c, levelMatrix)
+	
+	def findUniquePaths(self, levelMatrix):
+		r,c = len(levelMatrix),len(levelMatrix[0])
+		return self.findUniquePathsHelper(0, 0, r, c, levelMatrix)
+
 	def generateManual(self, gameObj):
 		rowSize = gameObj.rowSize
 		colSize = gameObj.colSize
@@ -25,11 +38,13 @@ class Generators:
 		gameObj.startNewGame(size)
 
 	def generateLevel(self, size):
-		rulesObj = Rules()
+		# rulesObj = Rules()
 		levelMatrix = []
 		while(True):
 			levelMatrix = self.generateRandom(size)
-			if(rulesObj.isStructured(size, levelMatrix) == True):
+			# if(rulesObj.isStructured(size, levelMatrix) == True and self.findUniquePaths(levelMatrix) != 0):
+			# 	break
+			if(self.findUniquePaths(levelMatrix) != 0):
 				break
 		return levelMatrix
 	
